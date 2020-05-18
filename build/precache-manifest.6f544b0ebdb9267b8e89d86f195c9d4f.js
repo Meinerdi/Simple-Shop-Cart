8d0bb480f736870734e957e7f6cc80c17cabe628
Meinerdi <meinerdi@gmail.com>
2020-05-19 00:20:08 +0300

    build: added

---
+++ build/precache-manifest.6f544b0ebdb9267b8e89d86f195c9d4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d003f1bf79df5cbc333a7adf25dbb8b",
+    "url": "/Shop-from-Zenbit-course/index.html"
+  },
+  {
+    "revision": "b94f456323c6f6234ca1",
+    "url": "/Shop-from-Zenbit-course/static/css/main.7c9edd7a.chunk.css"
+  },
+  {
+    "revision": "d8c9e222cf5346a551f2",
+    "url": "/Shop-from-Zenbit-course/static/js/2.ae048b0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shop-from-Zenbit-course/static/js/2.ae048b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94f456323c6f6234ca1",
+    "url": "/Shop-from-Zenbit-course/static/js/main.60b8e5a9.chunk.js"
+  },
+  {
+    "revision": "8784112f41e37bc66563",
+    "url": "/Shop-from-Zenbit-course/static/js/runtime-main.a655d7ee.js"
+  }
+]);
